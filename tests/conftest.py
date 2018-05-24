dac0546fe521fbae51092e9730bad5604996d00d
Allen H <w84miracle@gmail.com>
2018-05-24 23:07:42 +0800

    fix indents

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,16 +7,16 @@ from reddiggit import create_app
 
 @pytest.fixture
 def app():
-	app = create_app({
-		'TESTING': True,
-	})
-	yield app
+    app = create_app({
+        'TESTING': True,
+    })
+    yield app
 
 @pytest.fixture
 def client(app):
-	return app.test_client()
+    return app.test_client()
 
 
 @pytest.fixture
 def runner(app):
-	return app.test_cli_runner()
+    return app.test_cli_runner()
