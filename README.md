fd41c6709631719a53f463468b6d0bc535aa1806
Allen H <w84miracle@gmail.com>
2018-05-24 23:02:24 +0800

    code comments and README for explanations

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # reddiggit
-A Reddit / Digg clone with upvote and downvotes
+A Reddit / Digg clone with upvote and downvotes.
+Build with Flask.
+
+## Prerequisites
+To build and run this application, you will need:
+
+1.  Python version 2.7.9 (or higher)
+2. [Virtualenv](https://virtualenv.pypa.io/en/stable/)
+
+----
+## Build Instructions
+    git clone https://github.com/w84miracle/reddiggit
+    cd reddiggit/
+    virtualenv venv
+    . venv/bin/activate
+    pip install -r requirements.txt
+    export FLASK_APP=reddiggit
+    export FLASK_APP=development
+    flask run
+
+Visit [http://127.0.0.1:5000/](http://127.0.0.1:5000/)
+
+## Documentation
+* Both user and topic field can't be empty.
+* Topic should not exceeds 255 characters, which is handled in both frontend and backend.
+* Like Reddit, user can upvote/downvote a topic by clicking arrows on the left of each topics.
+* In-memory: All posts are store in a Python list.  **Hence all topics will disappear after the application restarts.**
+* Posts will be given a unique id on submissions to identify each post. Which is a base 36 encoding text. (Like Reddit's [fullnames](https://www.reddit.com/dev/api/))
+* Sorting may take a some time when the list getting bigger. So  instead of sorting the list each time user reaches index page, it will only sort on each submission and vote happens.
+
+----
+##Testing
+run pytest
--- reddiggit/post.py
+++ reddiggit/post.py
@@ -6,6 +6,17 @@ import base36
 bp = Blueprint('post', __name__, template_folder='templates')
 
 class Post(object):
+    """Class for storing informations from post that users submitted.
+    Attributes:
+        post_id (str): A base36 encoding (same as Reddit) text which
+            represents serial number of each posts on created.
+        author (str): User who submitted this post.
+        topic (str): The topic from user's input, should not exceeds 255
+            characters according to the requirement.
+        votes (int): The total number of upvote - downvote, default is 0.
+        create_time (str): The string of datetime when user submmit this post.
+    """
+
     def __init__(self, post_id, author, topic, votes=0):
         self.post_id = post_id
         self.author = author
@@ -14,26 +25,31 @@ class Post(object):
         self.create_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     def __lt__(self, other):
+        """Make instances of this class sortable by it's upvote."""
         return self.votes < other.votes
 
     def __eq__(self, other):
         return self.votes == other.votes
 
+# 'posts' is a list contains all user posts in-memory.
 posts=[]
 
 def get_post_by_id(post_id):
+    """Return a post object for a given post_id."""
     global posts
     for p in posts:
         if p.post_id == str(base36.loads(post_id)):
             break
     else:
         p = None
+
     return p
 
 @bp.route('/')
 def index():
     try:
-        return render_template('index.html', posts=posts)
+        # Render index page with top 20 upvotes posts.
+        return render_template('index.html', posts=posts[:20])
     except TemplateNotFound:
         abort(404)
 
