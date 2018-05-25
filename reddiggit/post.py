6bd3e2e2f748586fee3a7f75d166b5ad03e45ef8
Allen H <w84miracle@gmail.com>
2018-05-25 22:18:31 +0800

    add post/vote function with basic functional UI.

--- reddiggit/post.py
+++ reddiggit/post.py
@@ -1,36 +1,60 @@
-from flask import Blueprint, render_template, abort, g, request
+from flask import Blueprint, flash, redirect, url_for, render_template, abort, g, request
 from jinja2 import TemplateNotFound
+from datetime import datetime
 import base36
 
-bp = Blueprint('topic', __name__, template_folder='templates')
+bp = Blueprint('post', __name__, template_folder='templates')
 
 class Post(object):
-	def __init__(self, post_id, author, topic, votes=0):
-		self.post_id = post_id
-		self.author = author
-		self.topic = topic
-		self.votes = votes
+    def __init__(self, post_id, author, topic, votes=0):
+        self.post_id = post_id
+        self.author = author
+        self.topic = topic
+        self.votes = votes
+        self.create_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    def __lt__(self, other):
+        return self.votes < other.votes
+
+    def __eq__(self, other):
+        return self.votes == other.votes
+
 posts=[]
 
+def get_post_by_id(post_id):
+    global posts
+    for p in posts:
+        if p.post_id == str(base36.loads(post_id)):
+            break
+    else:
+        p = None
+    return p
+
 @bp.route('/')
-def show():
+def index():
     try:
-        return render_template('index.html',posts=posts)
+        return render_template('index.html', posts=posts)
     except TemplateNotFound:
         abort(404)
 
 @bp.route('/submit', methods=['GET', 'POST'])
 def add_post():
-	if request.method == 'POST':
-		posts.append( Post(base36.dumps(len(posts)), 'allenh', "Post No.%d" % (len(posts)+1)))
-		return 'Post added!'
+    if request.method == 'POST':
+        #Topic should not exceed 255 characters.
+        if len(request.values['topic']) > 255 :
+            abort(400)
+        new_post = Post(base36.dumps(len(posts)), request.values['author'], request.values['topic'])
+        posts.append(new_post)
+        posts.sort(reverse=True)
+        flash("Post %s added" % new_post.post_id)
+    return redirect(url_for('.index'))
 
 @bp.route('/<post_id>/<action>')
 def vote_post(post_id, action):
-	if action=='upvote':
-		return 'post %s +1.' % post_id
-	elif action=='downvote':
-		return 'post %s -1.' % post_id
-
-
-
+    p = get_post_by_id(post_id)
+    if action=='upvote':
+        p.votes +=1
+    elif action=='downvote':
+        p.votes -=1
+    posts.sort(reverse=True)
+    return redirect(url_for('.index'))
