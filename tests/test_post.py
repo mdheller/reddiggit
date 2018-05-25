434d72b129f97abd4c7849b70c8b646172454c2c
Allen H <w84miracle@gmail.com>
2018-05-25 22:33:34 +0800

    Add code comments and README

--- tests/test_post.py
+++ tests/test_post.py
@@ -2,16 +2,28 @@ from reddiggit import post
 
 def test_submit(client, app):
     assert len(post.posts) == 0 
-    response = client.post('/post/submit')
-    assert response.data == b'Post added!'
+    response = client.post('/post/submit', data=dict(
+        author='test_submit',
+        topic='Pisuke'
+    ))
+    assert response.status_code == 302
     assert len(post.posts) == 1
 
 def test_upvote(client):
-    response = client.post('/post/submit')
+    response = client.post('/post/submit', data=dict(
+        author='test_upvote',
+        topic='Usagi'
+    ))
+    assert response.status_code == 302
+    a=[i.votes for i in filter(lambda x: x.topic=='Usagi' ,post.posts)]
+    print a[0]
     rv = client.get('/post/0/upvote')
-    assert rv.data == b'post 0 +1.'
+    assert rv.status_code== 302
 
 def test_downvote(client):
-    response = client.post('/post/submit')
+    response = client.post('/post/submit', data=dict(
+        author='test_downvote',
+        topic='Pikachu'
+    ))
     rv = client.get('/post/0/downvote')
-    assert rv.data == b'post 0 -1.'
+    assert rv.status_code == 302
