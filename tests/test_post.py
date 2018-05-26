e6bf3f74677ad6f9b63d71bf4f1839681c7bdfb4
Allen H <w84miracle@gmail.com>
2018-05-26 17:34:36 +0800

    update code comments

--- tests/test_post.py
+++ tests/test_post.py
@@ -5,19 +5,19 @@ from reddiggit import post
 
 @pytest.mark.usefixtures('client')
 class TestPostFunctions:
-    
+
     def test_submit(self, client):
-        """Test if allow user to upvote multiple times."""
+        """Test submit post."""
         assert len(post.posts) == 0
         response = client.post('/post/submit', data=dict(
             author='test_submit',
             topic='Pisuke'
         ), follow_redirects=True)
-        
+
         #Check if topic in list.
-        assert 'Pisuke' in response.data 
+        assert 'Pisuke' in response.data
         assert len(post.posts) == 1
-        
+
 @pytest.mark.usefixtures('client')
 class TestVoteFuntions:
 
@@ -31,12 +31,12 @@ class TestVoteFuntions:
         #Get post_id from flash message.
         p_id = re.search('Post added with id (\w)', rv.data).group(1)
         p = post.get_post_by_id(p_id)
-        
+
         #Check if upvote increased.
         assert p.votes == 0
         upvote = client.get('/post/%s/upvote' % p_id)
         assert upvote.status_code== 302
-        assert p.votes == 1 
+        assert p.votes == 1
 
     def test_downvote(self, client):
         """Test downvote function."""
@@ -44,14 +44,14 @@ class TestVoteFuntions:
             author='test_downvote',
             topic='Pikachu'
         ), follow_redirects=True)
-        
+
         p_id = re.search('Post added with id (\w)', rv.data).group(1)
         p = post.get_post_by_id(p_id)
-        
+
         assert p.votes == 0
         downvote = client.get('/post/%s/downvote' % p_id)
         assert downvote.status_code == 302
-        assert p.votes == -1 
+        assert p.votes == -1
 
 
     def test_multi_upvote(self, client):
@@ -60,10 +60,10 @@ class TestVoteFuntions:
             author='test_multi_upvote',
             topic='Multiple upvotes'
         ), follow_redirects=True)
-        assert 'Multiple upvotes' in rv.data 
+        assert 'Multiple upvotes' in rv.data
         p_id = re.search('Post added with id (\w)', rv.data).group(1)
         p = post.get_post_by_id(p_id)
-        
+
         assert p.votes == 0
         for i in range(100):
            upvote = client.get('/post/%s/upvote' % p_id)
@@ -76,15 +76,15 @@ class TestVoteFuntions:
             author='test_multi_downvote',
             topic='Multiple downvotes'
         ), follow_redirects=True)
-        assert 'Multiple downvotes' in rv.data 
+        assert 'Multiple downvotes' in rv.data
         p_id = re.search('Post added with id (\w)', rv.data).group(1)
         p = post.get_post_by_id(p_id)
-        
+
         assert p.votes == 0
         for i in range(100):
            upvote = client.get('/post/%s/downvote' % p_id)
            assert upvote.status_code== 302
-           assert p.votes == -(i+1)        
+           assert p.votes == -(i+1)
 
 @pytest.mark.usefixtures('client')
 class TestSortFunctions:
@@ -100,7 +100,7 @@ class TestSortFunctions:
 
             #Get post_id from flash message.
             p_id = re.search('Post added with id (\w)', response.data).group(1)
-            
+
             #Upvote posts 'N' times if it's the Nth post.
             for j in range(i):
                 rv = client.get('/post/%s/upvote' % p_id )
@@ -114,16 +114,16 @@ class TestSortFunctions:
     def test_sort_after_submit(self, client):
         """Test if new posts are sorted as well."""
         assert len(post.posts) == 0
-        
+
         for i in range(2):
             client.post('/post/submit', data=dict(
                 author='sort_test',
                 topic='No.%d' % i
             ), follow_redirects=True)
-        
+
         client.get('/post/0/upvote')
         client.get('/post/1/downvote')
-        
+
         response = client.post('/post/submit', data=dict(
                 author='sort_test',
                 topic='this topic should between post 0 and post 1'
